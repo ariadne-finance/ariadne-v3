d37e42d0490d9bd5474ea740d0a996209c2ab1c9
Egor Egorov <me@egorfine.com>
2024-04-09 13:24:19 +0200

    those are view functions

--- contracts/interfaces/agave/ISavingsXDaiAdapter.sol
+++ contracts/interfaces/agave/ISavingsXDaiAdapter.sol
@@ -1,6 +1,6 @@
 interface ISavingsXDaiAdapter {
-    function sDAI() external returns (address);
-    function wxdai() external returns (address);
+    function sDAI() view external returns (address);
+    function wxdai() view external returns (address);
     function deposit(uint256 assets, address receiver) external returns (uint256);
     function redeem(uint256 shares, address receiver) external returns (uint256);
 }
