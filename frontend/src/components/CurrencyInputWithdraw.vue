1fe8d5f72cd075f7199452acf3df159ecc34583b
Egor Egorov <me@egorfine.com>
2024-04-15 20:32:52 +0200

    removed close button

--- frontend/src/components/CurrencyInputWithdraw.vue
+++ frontend/src/components/CurrencyInputWithdraw.vue
@@ -19,14 +19,6 @@
         class="inline-flex w-full justify-center items-center rounded-m p-4 h-full text-sm font-medium text-primary-400 focus:ring-0 focus-visible:outline-none"
         :disabled="disabled"
       >
-        <div v-if="amount" class="pr-4" @click.stop="reset">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-6 h-6"
-            fill="currentColor"
-            viewBox="0 0 256 256"
-          ><path d="M165.66,101.66,139.31,128l26.35,26.34a8,8,0,0,1-11.32,11.32L128,139.31l-26.34,26.35a8,8,0,0,1-11.32-11.32L116.69,128,90.34,101.66a8,8,0,0,1,11.32-11.32L128,116.69l26.34-26.35a8,8,0,0,1,11.32,11.32ZM232,128A104,104,0,1,1,128,24,104.11,104.11,0,0,1,232,128Zm-16,0a88,88,0,1,0-88,88A88.1,88.1,0,0,0,216,128Z" /></svg>
-        </div>
         <div class="text-2xl mr-1">[</div>
         <div v-if="selectedWithdrawToken" class="flex flex-nowrap gap-x-2 items-center pr-2">
           <div class="shrink-0 inline-flex items-center justify-center dark:opacity-90">
