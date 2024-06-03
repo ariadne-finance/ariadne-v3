e25be458b8853c596b4c624cae40c2da675a8ef6
Egor Egorov <me@egorfine.com>
2024-06-03 10:25:50 +0200

    build

---
+++ frontend-dist/assets/index-D3atbj_j.js
@@ -0,0 +1,10 @@
+import{bF as r,bb as t,bB as s,bM as i,bO as n,bP as b,bQ as f,bL as d,bG as c,bK as l,bJ as I,bN as h,bH as m,bI as p}from"./index-DQjQcTik.js";const a=`
+@font-face {
+  font-family: 'Inter';
+  font-style:  normal;
+  font-weight: 300 600;
+  font-display: swap;
+  src: url("https://rsms.me/inter/font-files/InterVariable.woff2") format("woff2-variations");
+}
+`;export{a as InterVar,r as ProviderRpcError,t as ProviderRpcErrorCode,s as bigIntToHex,i as chainIdToViemImport,n as chainIdValidation,b as chainNamespaceValidation,f as chainValidation,d as createDownloadMessage,c as createEIP1193Provider,l as ethToWeiBigInt,I as isAddress,h as validate,m as weiHexToEth,p as weiToEth};
+//# sourceMappingURL=index-D3atbj_j.js.map
